fdb4a9bf26d977b6f3b5885f40b9037f8a7f5b95
satputraya <satputraya@gmail.com>
2024-02-04 21:17:11 +0700

    update: rename function

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,11 +1,11 @@
 import bcrypt from "bcrypt";
 
 export class Password {
-  static hash = async (password: string): Promise<string> => {
+  static hashAsync = async (password: string): Promise<string> => {
     return await bcrypt.hash(password, 12);
   };
 
-  static verify = async (
+  static verifyAsync = async (
     password: string,
     hashedPassword: string
   ): Promise<boolean> => {
